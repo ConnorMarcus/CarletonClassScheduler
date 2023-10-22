dc433b13523e65798421033316332244fb3efe15
Noah Hammoud <noah.hammoud01@gmail.com>
2023-10-22 12:38:13 -0400

    added docker pipeline and clean up script

---
+++ WebScraping/scripts/image_clean_up.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+
+repository_name = os.environ.get('REPO_NAME')
+
+def delete_all_ecr_images(repository_name):
+    ecr = boto3.client('ecr')
+
+    # List the images in the repository
+    images = ecr.list_images(repositoryName=repository_name).get('imageIds', [])
+
+    if not images:
+        print("No images found in the repository.")
+        return
+
+    # Batch delete the images
+    ecr.batch_delete_image(repositoryName=repository_name, imageIds=images)
+    
+    print("Deleted all image(s)")
+
+if __name__ == '__main__':
+    delete_all_ecr_images(repository_name)
