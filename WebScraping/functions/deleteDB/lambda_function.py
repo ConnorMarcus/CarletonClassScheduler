7a584bc80846f605d7a740417dac8ad851593b31
Noah Hammoud <noah.hammoud01@gmail.com>
2023-12-22 11:45:13 -0500

    Created db insert and delete functions

---
+++ WebScraping/functions/deleteDB/lambda_function.py
@@ -0,0 +1,13 @@
+import boto3
+
+TABLE_NAME = "carleton-courses"
+
+def lambda_handler(event: dict, context: dict) -> str:
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(TABLE_NAME)
+    
+    # Delete the db and wait until its completed
+    table.delete()
+    table.wait_until_not_exists()
+    
+    return f"{TABLE_NAME} DB successfully deleted!"
