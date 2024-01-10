27bcf48540d63abed49a9d795dcec9f10115349e
muad-gush <muad19@gmail.com>
2024-01-10 18:36:44 -0500

    Added create db lambda function with test

---
+++ WebScraping/functions/createTable/lambda_function.py
@@ -0,0 +1,48 @@
+import boto3
+
+TABLE_NAME = "carleton-courses"
+
+def lambda_handler(event: dict, context: dict) -> str:
+    '''
+    Creates an Amazon DynamoDB table that is used to store carleton course
+    information.
+
+    Parameters:
+    event: event that triggers the Lambda function.
+    context: the Lambda function execution context.
+
+    Returns: 
+    - str: A message indicating that the DynamoDB table has been successfully created.
+    '''
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.create_table(TABLE_NAME,
+            KeySchema=[
+                {
+                    'AttributeName': 'Subject', 
+                    'KeyType': 'HASH' # Partition Key
+                },
+                {
+                    'AttributeName': 'Term',
+                    'KeyType': 'RANGE' # Sort Key 
+                }
+            ], 
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'Subject',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'Term',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+    )
+
+    # Wait until Database is created before returning
+    table.wait_until_exists()
+    
+    return f"Table {TABLE_NAME} successfully created!"
---
+++ WebScraping/tests/unit/test_create_db.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch, MagicMock
+from WebScraping.functions.createTable.lambda_function import *
+
+def test_lambda_handler():
+    with patch("boto3.resource") as mock_resource:
+        expected_table_name = "carleton-courses"
+        mock_dynamodb = MagicMock()
+        mock_table = MagicMock()
+        mock_resource.return_value = mock_dynamodb
+        mock_dynamodb.create_table.return_value = mock_table
+        
+        # Running the actual lambda handler
+        result = lambda_handler(event={}, context={})
+
+        mock_resource.assert_called_with("dynamodb")
+        mock_dynamodb.create_table.assert_called_with(
+            expected_table_name,
+            KeySchema=[
+                {'AttributeName': 'Subject', 'KeyType': 'HASH'},
+                {'AttributeName': 'Term', 'KeyType': 'RANGE'}
+            ],
+            AttributeDefinitions=[
+                {'AttributeName': 'Subject', 'AttributeType': 'S'},
+                {'AttributeName': 'Term', 'AttributeType': 'S'}
+            ],
+            ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10}
+        )
+        mock_table.wait_until_exists.assert_called_once()
+        assert result == f"Table {expected_table_name} successfully created!"
