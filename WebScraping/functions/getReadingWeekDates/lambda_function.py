b60f043bdf2d99c884e1636671ed0ff2030afbcc
Noah Hammoud <noah.hammoud01@gmail.com>
2024-02-09 20:49:51 -0500

    Created the "getReadingWeekDates" function
    Co-Authored-By: Muad Almangush <122187705+muad-gush@users.noreply.github.com>

---
+++ WebScraping/functions/getReadingWeekDates/lambda_function.py
@@ -0,0 +1,140 @@
+import json
+import boto3
+from datetime import datetime, timedelta
+import urllib3
+from bs4 import BeautifulSoup
+from typing import Tuple
+
+BASE_URL = "https://calendar.carleton.ca/academicyear"
+HTTP = urllib3.PoolManager()
+BUCKET_NAME = "carletonschedulingtool"
+KEY_PATH = "web-scraping-stepfunction/terms_courses.json"
+
+def lambda_handler(event: dict, context: dict) -> str:
+    parser = get_parser()
+    terms_courses_dict = get_terms_courses_dict()
+    
+    for term in terms_courses_dict.keys():
+        name, year = term.split()
+        term_search_str = f"{name.upper()} TERM {year}"
+        term_tbody = parser.find("td", string=term_search_str).find_parent("tbody")
+
+        if name == "Fall" or name == "Winter":
+            break_lookup_str = f"{name} break"
+            date = term_tbody.find(lambda tag: tag.name == "td" and break_lookup_str in tag.text).find_previous('td').text.strip()
+            
+            start_week, end_week, next_week = get_formatted_fall_or_winter_dates(date)
+
+        else:
+            start_lookup_str ="Last day of early summer classes"
+            start_date = term_tbody.find(lambda tag: tag.name == "td" and start_lookup_str in tag.text).find_previous('td').text.strip()
+            
+            end_lookup_str = "Late summer classes begin"
+            end_date = term_tbody.find(lambda tag: tag.name == "td" and end_lookup_str in tag.text).find_previous('td').text.strip()
+            
+            start_week, end_week, next_week = get_formatted_summer_dates(start_date, end_date)
+
+
+        terms_courses_dict[term]["ReadingWeekStart"] = start_week
+        terms_courses_dict[term]["ReadingWeekEnd"] = end_week
+        terms_courses_dict[term]["ReadingWeekNext"] = next_week
+        
+    return write_terms_courses_to_s3(terms_courses_dict)
+
+
+def get_parser() -> BeautifulSoup:
+    '''
+    Gets the bs4 html parser.
+
+    Returns:
+    BeautifulSoup: html parser object.
+    '''
+    req = HTTP.request("GET", BASE_URL)
+    html = req.data.decode("utf-8")
+    return BeautifulSoup(html, "html.parser")
+
+
+def get_terms_courses_s3_object() -> object:
+    '''
+    Gets the terms courses s3 file object.
+
+    Returns:
+    S3 Object: The s3 file object.
+    '''
+    s3 = boto3.resource("s3")
+    return s3.Object(BUCKET_NAME, KEY_PATH)
+    
+
+def get_terms_courses_dict() -> dict:
+    '''
+    Gets the list of classes from s3.
+    
+    Returns:
+    dict: terms course dict.
+    '''
+    terms_courses_file = get_terms_courses_s3_object()
+    return json.load(terms_courses_file.get()["Body"])
+
+
+def get_formatted_fall_or_winter_dates(date_str: str) -> Tuple[str, str, str]:
+    '''
+    Formats fall or winter term reading week dates. 
+
+    Parameters:
+    date_str: The string of the data that is to be formattted.
+
+    Returns:
+    Tuple: the formatted date start, end and next dates.
+    '''
+    month_str, start_end_str, year_str = date_str.split()
+    month = datetime.strptime(month_str, '%B').month
+    start_day = int(start_end_str.split('-')[0])
+    end_day = int(start_end_str.split('-')[1][:-1])
+    year = int(year_str)
+
+    start_date = (datetime(year, month, start_day) + timedelta(days=-3)).date().isoformat() 
+    end_date =(datetime(year, month, end_day) + timedelta(days=3)).date().isoformat() 
+    next_date = (datetime(year, month, end_day) + timedelta(days=10)).date().isoformat() 
+
+    return start_date, end_date, next_date
+
+
+def get_formatted_summer_dates(start_date: str, end_date: str) -> Tuple[str, str, str]:
+    '''
+    Formats the summer term reading week dates. 
+
+    Parameters:
+    start_date: The string start date that is to be formattted
+    start_date: The string end date that is to be formattted
+
+    Returns:
+    Tuple: the formatted date start, end and next dates.
+    '''
+    parsed_format = "%B %d, %Y"
+    formatted_format = "%Y-%m-%d"
+
+    parsed_start_date = datetime.strptime(start_date, parsed_format)
+    formatted_start_date = parsed_start_date.strftime(formatted_format)
+
+    parsed_end_date = datetime.strptime(end_date, parsed_format)
+    formatted_end_date = parsed_end_date.strftime(formatted_format)
+
+    return formatted_start_date, formatted_end_date, formatted_end_date
+
+
+def write_terms_courses_to_s3(updated_terms_courses: dict[str, str]) -> str:
+    '''
+    Writes updated terms courses file to S3.
+
+    Parameters: 
+    updated_terms_courses: dictionary of containing terms with courses and reading week dates.
+
+    Returns:
+    str: Message indicating the terms and courses file has been updated.
+    '''
+    terms_courses_file = get_terms_courses_s3_object()
+    terms_courses_file.put(Body=(bytes(json.dumps(updated_terms_courses).encode("UTF-8"))))
+    
+    return {
+        "Response": json.dumps("Reading week dates added to Terms and Course file!")
+    }
---
+++ WebScraping/tests/unit/test_get_reading_week_dates.py
@@ -0,0 +1,148 @@
+from WebScraping.functions.getReadingWeekDates.lambda_function import *
+from unittest.mock import MagicMock, patch
+from botocore.stub import Stubber
+
+FILE_PATH = "WebScraping.functions.getReadingWeekDates.lambda_function"
+
+TERMS_COURSES_DICT = {
+    "Fall 2023": {
+        "Classes": ["SYSC 4001", "SYSC 4001 A"]
+    },
+    "Winter 2024": {
+        "Classes": ["SYSC 3200", "SYSC 3200 A"]
+    },
+    "Summer 2024": {
+        "Classes": ["SYSC 2006", "SYSC 2006 A"]
+    }
+}
+
+UPDATED_TERMS_COURSES_DICT = {
+    "Fall 2023": {
+        "Classes": ["SYSC 4001", "SYSC 4001 A"],
+        "ReadingWeekStart": "2023-10-20", 
+        "ReadingWeekEnd": "2023-10-30", 
+        "ReadingWeekNext": "2023-11-06"
+    }
+}
+
+MOCK_HTML = """
+    <tbody>
+        <tr class="even firstrow">
+            <td class="column0">FALL TERM 2023</td> 
+            <td class="column1"></td> 
+        </tr>
+        <tr class="odd">
+            <td class="column0">October 23-27, 2023</td> 
+            <td class="column1">Fall break, no classes.</td> 
+        </tr>
+    </tbody>
+    <tbody>
+        <tr class="even firstrow">
+            <td class="column0">WINTER TERM 2024</td> 
+            <td class="column1"></td> 
+        </tr>
+        <tr class="odd">
+            <td class="column0">February 19-23, 2024</td> 
+            <td class="column1">Winter break, no classes. </td> 
+        </tr>
+    </tbody>
+    <tbody>
+        <tr class="even firstrow">
+            <td class="column0">SUMMER TERM 2024</td> 
+            <td class="column1"></td> 
+        </tr>
+        <tr class="odd">
+            <td class="column0">June 18, 2024</td> 
+            <td class="column1">Last day of early summer classes. (NOTE: full summer classes resume July 2.)</td> 
+        </tr>
+        <tr class="odd">
+            <td class="column0">July 2, 2024</td> 
+            <td class="column1">Late summer classes begin and full summer classes resume. </td> 
+        </tr>
+    </tbody>
+"""
+
+def test_lambda_handler():
+    with patch(f'{FILE_PATH}.get_parser') as mock_get_parser, \
+         patch(f'{FILE_PATH}.get_terms_courses_dict') as mock_get_terms_courses_dict, \
+         patch(f'{FILE_PATH}.write_terms_courses_to_s3') as mock_write_terms_courses_to_s3:
+        
+        mock_get_parser.return_value = BeautifulSoup(MOCK_HTML, "html.parser")
+        mock_get_terms_courses_dict.return_value = TERMS_COURSES_DICT
+        mock_write_terms_courses_to_s3.return_value = {"Response": json.dumps("Reading week dates added to Terms and Course file!")}
+
+        lambda_handler({}, {})
+
+        mock_get_parser.assert_called_once()
+        mock_get_terms_courses_dict.assert_called_once()
+        mock_write_terms_courses_to_s3.assert_called_once()
+
+
+def test_get_parser():
+    test_html = "<html><body>Mocked HTML</body></html>"
+    expected_bs4 = BeautifulSoup(test_html, "html.parser")
+
+    with patch("urllib3.PoolManager.request") as mock_pool_manager:
+        mock_response = MagicMock()
+        mock_response.data = test_html.encode()
+        mock_pool_manager.return_value = mock_response
+
+        results = get_parser()
+        assert results == expected_bs4
+
+
+def test_get_terms_courses_s3_object():
+    s3_client = boto3.client("s3")
+    s3_stubber = Stubber(s3_client)
+
+    expected_params = {"Bucket": BUCKET_NAME, "Key": KEY_PATH}
+    s3_stubber.add_response("get_object", {"Body": "test content"}, expected_params)
+
+    with s3_stubber:
+        result = get_terms_courses_s3_object()
+
+    assert result.bucket_name == BUCKET_NAME
+    assert result.key == KEY_PATH
+
+
+def test_get_terms_courses_dict():
+    s3_object = MagicMock()
+    class_dict = json.dumps(TERMS_COURSES_DICT)
+    s3_object.get.return_value = {"Body": MagicMock(read=lambda: class_dict)}
+
+    with patch(f"{FILE_PATH}.get_terms_courses_s3_object", return_value=s3_object) as mock_s3:
+        result = get_terms_courses_dict()
+        mock_s3.assert_called_once_with()
+
+        assert result == TERMS_COURSES_DICT
+
+
+def test_get_formatted_fall_or_winter_dates():
+    results = get_formatted_fall_or_winter_dates("October 23-27, 2023")
+    expected_dates = ("2023-10-20", "2023-10-30", "2023-11-06")
+    assert results == expected_dates
+
+    results = get_formatted_fall_or_winter_dates("February 19-23, 2024")
+    expected_dates = ("2024-02-16", "2024-02-26", "2024-03-04")
+    assert results == expected_dates
+
+
+def test_get_formatted_summer_dates():
+    results = get_formatted_summer_dates("June 18, 2024", "July 2, 2024")
+    expected_dates = ("2024-06-18", "2024-07-02", "2024-07-02")
+    assert results == expected_dates
+
+
+def test_write_terms_courses_to_s3():
+    s3_object = MagicMock()
+    s3_object.put = MagicMock()
+
+    with patch(f"{FILE_PATH}.get_terms_courses_s3_object", return_value=s3_object) as mock_s3:
+
+        result = write_terms_courses_to_s3(UPDATED_TERMS_COURSES_DICT)
+        mock_s3.assert_called_once_with()
+
+        expected_result = bytes(json.dumps(UPDATED_TERMS_COURSES_DICT).encode("UTF-8"))
+        s3_object.put.assert_called_once_with(Body=expected_result)
+
+        assert result == {"Response": json.dumps("Reading week dates added to Terms and Course file!")}
