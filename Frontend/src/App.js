adef58bc32e862a6f005d85a8301b7cda13e59a8
vahido9 <vayeeded@gmail.com>
2024-03-10 20:24:07 -0400

    Added Footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import LandingPageComponent from './components/LandingPageComponent';
 import FormComponent from './components/FormComponent';
 import CalendarComponent from './components/CalendarComponent';
 import ErrorPage from './components/ErrorPage';
+import Footer from './components/Footer'
 
 const App = () => {
   const [displayCalendar, setDisplayCalendar] = useState(false);
@@ -11,14 +12,14 @@ const App = () => {
   const [scheduleCount, setScheduleCount] = useState(0);
   const [serverError, setServerError] = useState(false);
   const calendarRef = useRef();
-  
+
   useEffect(() => {
     if (displayCalendar && calendarRef.current) {
       calendarRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   });
 
-  const displayCalendarComponent = () => (displayCalendar ? <CalendarComponent title={term} events={schedules} scheduleCount={scheduleCount} setScheduleCount={setScheduleCount} ref={calendarRef}/> : null);
+  const displayCalendarComponent = () => (displayCalendar ? <CalendarComponent title={term} events={schedules} scheduleCount={scheduleCount} setScheduleCount={setScheduleCount} ref={calendarRef} /> : null);
 
   if (serverError) {
     return <ErrorPage />
@@ -27,8 +28,9 @@ const App = () => {
   return (
     <div>
       <LandingPageComponent />
-      <FormComponent setDisplayCalendar={setDisplayCalendar} setTerm={setTerm} setSchedules={setSchedules} setScheduleCount={setScheduleCount} setServerError={setServerError}/>
+      <FormComponent setDisplayCalendar={setDisplayCalendar} setTerm={setTerm} setSchedules={setSchedules} setScheduleCount={setScheduleCount} setServerError={setServerError} />
       {displayCalendarComponent()}
+      <Footer />
     </div>
   );
 };
---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,46 @@
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import { GitHub } from '@mui/icons-material';
+import '../styles/Footer.css';
+
+const theme = responsiveFontSizes(createTheme());
+
+const Footer = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Box className="footer">
+                <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <Typography align="left">
+                            &copy; 2024 CarletonClassScheduler. All rights reserved.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm="auto" container justifyContent="center" style={{ textAlign: 'right' }}>
+                        <Button
+                            href="https://github.com/ConnorMarcus/CarletonClassScheduler"
+                            target="_blank"
+                            variant="outlined"
+                            size="medium"
+                            sx={{
+                                color: 'black',
+                                borderColor: 'black',
+                                transition: 'opacity 0.3s ease-in-out',
+                                maxWidth: 'fit-content',
+                                '&:hover': {
+                                    opacity: 0.7,
+                                    borderColor: 'black'
+                                },
+                                alignContent: 'right',
+                            }}
+                            startIcon={<GitHub />}
+                        >
+                            View on GitHub
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Box>
+        </ThemeProvider>
+    );
+};
+
+export default Footer;
