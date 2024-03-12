3af9d9e22cc97c3e10c963c211530e60d2efe9d1
Noah Hammoud <noah.hammoud01@gmail.com>
2024-03-12 15:09:26 -0400

    Updated calendar to use MUI

--- Frontend/src/components/Calendar.js
+++ Frontend/src/components/Calendar.js
@@ -3,8 +3,12 @@ import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import rrulePlugin from '@fullcalendar/rrule';
 import { getCourseTime } from '../common/utils';
-import { Alert, Box, Modal, Snackbar } from '@mui/material';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import { Alert, Box,  Button,  Modal, Snackbar, Typography } from '@mui/material';
+import { ArrowBackIos, ArrowForwardIos, ContentCopy } from '@mui/icons-material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import '../styles/Calendar.css';
+
 
 const Calendar = React.forwardRef(({ title, events, scheduleCount, setScheduleCount }, ref) => {
     const [openAlert, setOpenAlert] = useState(false);
@@ -93,62 +97,143 @@ const Calendar = React.forwardRef(({ title, events, scheduleCount, setScheduleCo
         setOpenModal(false);
     }
 
+    // Handle responsiveness
+    const theme = responsiveFontSizes(createTheme());
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const dayHeaderFormat = isSmallScreen ? { weekday: 'short' } : { weekday: 'long' };
+    const responsiveMargin = isSmallScreen ? '0' : '25px';
+    const responsivePadding = isSmallScreen ? '0 10px 0 10px' : '0 40px 20px 40px;';
+
     return (
-        <div id="calendar" ref={ref}>
-            <h2 id="calendar-title">{title}</h2>
-            <button className="crn-btn" type="button" onClick={copyCRNsToClipboard}>Export CRNs</button>
-            <p id="schedule-carrousel">
-                <input className="prev-next-btn" type="button" disabled={scheduleCount === 0} onClick={handlePrevClick} value="prev"></input>
-                <span id="schedule-count-text">Schedule {scheduleCount + 1}</span>
-                <input className="prev-next-btn" type="button" disabled={scheduleCount === events.length - 1} onClick={handleNextClick} value="next"></input>
-            </p>
-            <FullCalendar
-                plugins={[timeGridPlugin, rrulePlugin]}
-                initialView="timeGridWeek"
-                weekends={false}
-                slotDuration="00:30:00"
-                allDaySlot={false}
-                slotMinTime={"8:00:00"}
-                slotMaxTime={"21:00:00"}
-                dayHeaderFormat={{ weekday: 'short' }}
-                height="auto"
-                initialDate={earliestStartDate(events)}
-                events={events[scheduleCount]["sync"]}
-                eventClick={handleEventClick}
-                headerToolbar={{ end: 'prev,next' }}
-            />
-            <div className="async-courses">
-                {events[scheduleCount]["async"].length !== 0 && (<p>Courses without assigned meeting times</p>)}
-                {events[scheduleCount]["async"].length !== 0 && events[scheduleCount]["async"]?.map((course, index) => (
-                    <span key={index}><b className="async-course-name">{course.title}</b> - {course.crn}</span>)).reduce((prev, curr) => [prev, ', ', curr])
-                }
-            </div>
-
-            <Snackbar open={openAlert} autoHideDuration={5000} onClose={handleCloseAlert}>
-                <Alert onClose={handleCloseAlert} severity="info" style={{ fontSize: '1.25rem' }}>
-                    {alertMessage}
-                </Alert>
-            </Snackbar>
-
-            <Modal
-                open={openModal}
-                onClose={handleCloseModal}
-                aria-labelledby="modal-title"
-                aria-describedby="modal-description"
-            >
-                <Box sx={{ color: 'black', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: 'fit-content', width: 275, bgcolor: 'background.paper', border: '1px solid #BF122B', boxShadow: 24, p: 2, borderRadius: '10px', }}>
-                    <h2 id="modal-title">{eventDetails?.title}</h2>
-                    <h3>{eventDetails?.name}</h3>
-                    <p id="modal-description" style={{ textAlign: 'left' }}>
-                        <b>Time: </b>{eventDetails?.startTimeStr} - {eventDetails?.endTimeStr}<br />
-                        <b>Instructor: </b>{eventDetails?.instructor}<br />
-                        <b>CRN: </b>{eventDetails?.crn}<br />
-                        <b>Status: </b>{eventDetails?.status}<br />
-                        {eventDetails?.prereq && <><b>Prerequisites: </b>{eventDetails?.prereq}<br /></>}
-                    </p>
+        <ThemeProvider theme={theme}>
+            <Box id="calendar" ref={ref} sx={{ boxShadow: '10', margin: responsiveMargin, padding: responsivePadding }}>
+                <Typography variant='h4' sx={{ fontWeight: 'bold', marginY: '30px' }}>
+                    Schedules for {title}
+                </Typography>
+                <Box display="flex" justifyContent="center" alignItems="center" sx={{ margin: '20px 0' }}>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            background: '#BF122B',
+                            paddingY: '8px',
+                            color: 'white',
+                            '&:hover': {
+                                background: '#BF122B',
+                            },
+                            '&:disabled': {
+                                background: '#BF122B',
+                                color: 'white',
+                                opacity: '50%'
+                            }
+                        }}
+                        size="small"
+                        onClick={handlePrevClick}
+                        disabled={scheduleCount === 0}
+                        startIcon={<ArrowBackIos />}
+                    >
+                        prev
+                    </Button>
+                    <Typography variant='h6' sx={{ paddingX: '15px' }}>
+                        {scheduleCount + 1} of {events.length}
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            background: '#BF122B',
+                            paddingY: '8px',
+                            color: 'white',
+                            '&:hover': {
+                                background: '#BF122B',
+                            },
+                            '&:disabled': {
+                                background: '#BF122B',
+                                color: 'white',
+                                opacity: '50%'
+                            }
+                        }}
+                        size="small"
+                        onClick={handleNextClick}
+                        disabled={scheduleCount === events.length - 1}
+                        endIcon={<ArrowForwardIos />}
+                    >
+                        Next
+                    </Button>
                 </Box>
-            </Modal>
-        </div >
+                <FullCalendar
+                    plugins={[timeGridPlugin, rrulePlugin]}
+                    initialView="timeGridWeek"
+                    weekends={false}
+                    slotDuration="00:30:00"
+                    allDaySlot={false}
+                    slotMinTime={"8:00:00"}
+                    slotMaxTime={"21:00:00"}
+                    dayHeaderFormat={dayHeaderFormat}
+                    height="auto"
+                    initialDate={earliestStartDate(events)}
+                    events={events[scheduleCount]["sync"]}
+                    eventClick={handleEventClick}
+                    headerToolbar={{ end: 'prev,next' }}
+                />
+                <Box className="async-courses" display="flex" justifyContent="space-between" alignItems="center">
+                    <Box sx={{ textAlign: 'left' }}>
+                        {events[scheduleCount]["async"].length !== 0 && (
+                            <Typography variant="body1" sx={{ marginTop: '20px', marginBottom: '5px' }}>Courses without assigned meeting times</Typography>
+                        )}
+                        {events[scheduleCount]["async"].length !== 0 && (
+                            <Typography variant="body1">
+                                {events[scheduleCount]["async"]?.map((course, index) => (
+                                    <React.Fragment key={index}>
+                                        {index > 0 && ', '}
+                                        <b className="async-course-name">{course.title}</b> - {course.crn}
+                                    </React.Fragment>
+                                ))}
+                            </Typography>
+                        )}
+                    </Box>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            background: '#BF122B',
+                            color: 'white',
+                            '&:hover': {
+                                background: '#BF122B',
+                            },
+                            marginTop: '20px'
+                        }}
+                        size="medium"
+                        onClick={copyCRNsToClipboard}
+                        startIcon={<ContentCopy />}
+                    >
+                        Export CRNs
+                    </Button>
+                </Box>
+
+                <Snackbar open={openAlert} autoHideDuration={5000} onClose={handleCloseAlert}>
+                    <Alert onClose={handleCloseAlert} severity="info" style={{ fontSize: '1.25rem' }}>
+                        {alertMessage}
+                    </Alert>
+                </Snackbar>
+
+                <Modal
+                    open={openModal}
+                    onClose={handleCloseModal}
+                    aria-labelledby="modal-title"
+                    aria-describedby="modal-description"
+                >
+                    <Box sx={{ color: 'black', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: 'fit-content', width: 275, bgcolor: 'background.paper', border: '1px solid #BF122B', boxShadow: 24, p: 2, borderRadius: '10px', }}>
+                        <h2 id="modal-title">{eventDetails?.title}</h2>
+                        <h3>{eventDetails?.name}</h3>
+                        <p id="modal-description" style={{ textAlign: 'left' }}>
+                            <b>Time: </b>{eventDetails?.startTimeStr} - {eventDetails?.endTimeStr}<br />
+                            <b>Instructor: </b>{eventDetails?.instructor}<br />
+                            <b>CRN: </b>{eventDetails?.crn}<br />
+                            <b>Status: </b>{eventDetails?.status}<br />
+                            {eventDetails?.prereq && <><b>Prerequisites: </b>{eventDetails?.prereq}<br /></>}
+                        </p>
+                    </Box>
+                </Modal>
+            </Box >
+        </ThemeProvider>
     );
 });
 
