6745bbf7f5bc305aaf9d5670bd30fbc763b20f27
Noah Hammoud <noah.hammoud01@gmail.com>
2024-03-30 12:56:22 -0400

    Added accessibility checks to the e2e tests

--- Frontend/cypress.config.js
+++ Frontend/cypress.config.js
@@ -2,6 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+        table(message) {
+          console.table(message);
+          return null;
+        }
+      });
+    },
   },
 });
